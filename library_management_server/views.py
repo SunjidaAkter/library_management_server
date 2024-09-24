8f75baea16dd840a1b780f5c9b6929572a6c21bb
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-09-24 12:36:30 +0600

    first commit

--- library_management_server/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User 
-
-from rest_framework import viewsets
-from . import serializers
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
-    
