fca8eb5af056eb60be3acf2b186320b0fa72917f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-10-04 22:18:51 +0600

    mission accomplished 🔥🚀🔥

--- borrow/models.py
+++ borrow/models.py
@@ -9,8 +9,8 @@ BORROW_STATUS = [
 
 # Create your models here.
 class Borrow(models.Model):
-    borrower = models.ForeignKey(UserAccount, on_delete=models.DO_NOTHING, related_name='borrowers')
-    book = models.ForeignKey(Book, on_delete=models.DO_NOTHING, related_name='books')
+    borrower = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='borrowers')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='books')
     borrow_status = models.CharField(choices = BORROW_STATUS, max_length = 10, default = "Borrowed")
     balance_after_borrow=models.IntegerField(null=True, blank=True)
     balance_after_return=models.IntegerField(null=True, blank=True)
